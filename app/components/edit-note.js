36ba1f998cdccb09ec0bf4496c479f78fdf4f4f2
Adam Phillips <aadam21@gmail.com>
2018-05-11 14:21:30 -0400

    Tweaks

--- app/components/edit-note.js
+++ app/components/edit-note.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   actions: {
     saveNote: function() {
-      this.get('note').save();
+      this.get('note').save(); //Getting and saving an existing record results in a PUT
     },
     closeNote: function() {
       this.sendAction('close');
